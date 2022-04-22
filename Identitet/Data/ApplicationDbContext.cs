78d8e3bce1cbadd6110b41338efd26eacd45d5f5
PC <PC@DESKTOP-J1EM1QQ>
2022-04-22 18:17:16 +0200

    Add project files.

---
+++ Identitet/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identitet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        DbSet<Manager> Managers { get; set; }
+    }
+}
